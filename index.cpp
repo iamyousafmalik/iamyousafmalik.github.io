a1e1146c2eab4527df8bb4486709673a48858d47
iamyousafmalik <125479765+iamyousafmalik@users.noreply.github.com>
2023-02-18 21:17:02 +0500

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int a,b;
+  cout<<"Enter the First No : "<<endl;
+  cin>>a;
+  cout<<"Enter the Second No : "<<endl;
+  cin>>b;
+  cout<<a+b;
+  return 0;
+}
